d8d8647d7f52fda8d5fcd96cdd427681e6d9212c
KatrideRRR <akimadzhiametov@gmail.com>
2026-05-04 00:06:06 +0300

    for android commit: edit allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,15 +1,26 @@
 const allowedOrigins = [
+    // Local dev
     "http://localhost",
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:8080",
+    "http://localhost:8100",
+
+    // Capacitor mobile apps
+    "capacitor://localhost",
+    "ionic://localhost",
+
+    // Old / direct IP access
     "http://18.184.43.44:3001",
     "http://81.163.27.147:8080",
     "https://81.163.27.147:3001",
     "https://81.163.27.147:8080",
+
+    // Production domains
     "https://cargocamp.ru",
     "https://www.cargocamp.ru",
     "https://admin.cargocamp.ru",
+    "https://api.cargocamp.ru",
 ];
 
 module.exports = allowedOrigins;
